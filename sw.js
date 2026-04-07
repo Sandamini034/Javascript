72ac5b5b8e7dc3a18e62f28d02f0c92c009432f4
Honali Sandamini <hsandaminih@gmail.com>
2026-04-07 23:36:41 +0530

    modify the task5

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["task5.html"])
-        })
-    );
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll(["task5.html"]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-    console.log("fetch event", e.request.url);
+self.addEventListener("fetch", (e) => {
+  console.log("fetch event", e.request.url);
 });
